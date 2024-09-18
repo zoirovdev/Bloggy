3444e3accaa5c75c815ff06a9c0ba76336ad26e8
Abbos Zoirov <abboszoirov17@gmail.com>
2024-09-18 13:08:03 +0500

    add EssayList, EssayDetail and UserEssayView

--- essays/urls.py
+++ essays/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('list/', views.EssayList.as_view()),
+    path('detail/<int:pk>', views.EssayDetail.as_view()),
+    path('user-views/<int:pk>', views.UserEssayView.as_view()),
 ]
--- essays/views.py
+++ essays/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from .models import Essay
+from .serializers import EssaySerializer
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class EssayList(APIView):
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request):
+        serializer = EssaySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+    
+    def get(self, request):
+        essays = Essay.objects.all()
+        serializer = EssaySerializer(essays, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+class EssayDetail(APIView):
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def put(self, request, pk):
+        essay = Essay.objects.get(pk=pk)
+        serializer = EssaySerializer(essay, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    def get(self, reuqest, pk):
+        essay = Essay.objects.get(pk=pk)
+        serializer = EssaySerializer(essay)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, pk):
+        essay = Essay.objects.get(pk=pk)
+        essay.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class UserEssayView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        essays = Essay.objects.filter(author=pk)
+        serializer = EssaySerializer(essays, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    
+
+
